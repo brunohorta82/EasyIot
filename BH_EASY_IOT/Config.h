8818d0ca52b26923c3baabb39156b427f55232a3
Bruno Horta <brunohorta82@gmail.com>
2019-06-19 02:03:48 +0100

    v6.16

--- BH_EASY_IOT/Config.h
+++ BH_EASY_IOT/Config.h
@@ -1,5 +1,5 @@
 #define HARDWARE "onofre"
-#define FACTORY_TYPE "cover"  //suported types cover light
+#define FACTORY_TYPE "gate"  //suported types cover light
 #define FIRMWARE_VERSION 6.16
 #define FIRMWARE_VERSION_X "6x16"
 const String DEFAULT_NODE_ID = String(HARDWARE) +"-"+String(FACTORY_TYPE)+"-"+String(ESP.getChipId())+"-"+String(FIRMWARE_VERSION_X);
