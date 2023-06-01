4a1e91cdd8b352c299ffbd371b3f586331cdd24a
Bruno Horta <bruno.horta@avanceesoftware.com>
2023-06-01 16:31:45 +0100

    Update extra_script.py

--- EasyIot7/extra_script.py
+++ EasyIot7/extra_script.py
@@ -14,4 +14,4 @@ env.Append(
 )
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 
-env.Replace(PROGNAME="firmware_%s_%s" % (model , my_flags.get("CPPDEFINES")[1][1]))
+env.Replace(PROGNAME="firmware_%s_%s" % (model , my_flags.get("CPPDEFINES")[0][1]))
