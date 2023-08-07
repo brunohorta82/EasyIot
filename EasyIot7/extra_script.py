952ede21ad6eaaf2eb7c8f6c37b9a0be08effab5
Hauzman <56076331+Hauzman@users.noreply.github.com>
2023-08-07 22:20:08 +0300

    Add name to Bin file

--- EasyIot7/extra_script.py
+++ EasyIot7/extra_script.py
@@ -1,4 +1,4 @@
 Import("env")
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 
-env.Replace(PROGNAME="firmware_%s_%s" % (env["PIOENV"] , my_flags.get("CPPDEFINES")[0][1]))
+env.Replace(PROGNAME="OnOfre_firmware_%s_%s" % (env["PIOENV"] , my_flags.get("CPPDEFINES")[0][1]))
