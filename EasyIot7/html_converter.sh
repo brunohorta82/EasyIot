6cd0ee7a77168fb9def05bc4f15e95b30918b7c9
bruno horta <geral@bhonofre.pt>
2023-08-19 12:26:29 +0100

    auto converter css and js to hex

--- EasyIot7/html_converter.sh
+++ EasyIot7/html_converter.sh
@@ -1,6 +1,8 @@
 gzip -k  ../html/devices.html
 gzip -k  ../html/index.html   
 gzip -k  ../html/node.html
+gzip -k  ../html/js/index.js
+gzip -k  ../html/css/styles.min.css
 
 xxd -i -n devices_html ../html/devices.html.gz > include/DevicesHtml.h 
 sed -i "" "s/unsigned char index_html/unsigned char index_html PROGMEM/" include/DevicesHtml.h
@@ -14,6 +16,16 @@ xxd -i -n node_html ../html/node.html.gz > include/NodeHtml.h
 sed -i "" "s/unsigned char index_html/unsigned char index_html PROGMEM/" include/NodeHtml.h
 echo "#include <Arduino.h>\n\r$(cat include/NodeHtml.h)" > include/NodeHtml.h 
 
+xxd -i -n index_js ../html/js/index.js.gz > include/IndexJs.h 
+sed -i "" "s/unsigned char index_js/unsigned char index_js PROGMEM/" include/IndexJs.h
+echo "#include <Arduino.h>\n\r$(cat include/IndexJs.h)" > include/IndexJs.h 
+
+xxd -i -n styles_min_css ../html/css/styles.min.css.gz > include/StylesMinCss.h 
+sed -i "" "s/unsigned char styles_min_css/unsigned char styles_min_css PROGMEM/" include/StylesMinCss.h
+echo "#include <Arduino.h>\n\r$(cat include/StylesMinCss.h)" > include/StylesMinCss.h 
+
 rm -f  ../html/devices.html.gz
 rm -f  ../html/index.html.gz   
 rm -f  ../html/node.html.gz 
+rm -f  ../html/js/index.js.gz 
+rm -f  ../html/css/styles.min.css.gz 
