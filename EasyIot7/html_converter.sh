54421b25ec1f10c8be7022293881f72d9ee77432
bruno horta <geral@bhonofre.pt>
2023-08-19 12:54:08 +0100

    css mimify

--- EasyIot7/html_converter.sh
+++ EasyIot7/html_converter.sh
@@ -1,18 +1,22 @@
-gzip -k  ../html/devices.html
-gzip -k  ../html/index.html   
-gzip -k  ../html/node.html
+html-minifier --collapse-whitespace ../html/devices.html -o ../html/devices.min.html
+html-minifier --collapse-whitespace ../html/index.html -o ../html/index.min.html
+html-minifier --collapse-whitespace ../html/node.html -o ../html/node.min.html
+
+gzip -k  ../html/devices.min.html
+gzip -k  ../html/index.min.html   
+gzip -k  ../html/node.min.html
 gzip -k  ../html/js/index.js
 gzip -k  ../html/css/styles.min.css
 
-xxd -i -n devices_html ../html/devices.html.gz > include/DevicesHtml.h 
+xxd -i -n devices_html ../html/devices.min.html.gz > include/DevicesHtml.h 
 sed -i "" "s/unsigned char index_html/unsigned char index_html PROGMEM/" include/DevicesHtml.h
 echo "#include <Arduino.h>\n\r$(cat include/DevicesHtml.h)" > include/DevicesHtml.h 
 
-xxd -i -n index_html ../html/index.html.gz > include/IndexHtml.h 
+xxd -i -n index_html ../html/index.min.html.gz > include/IndexHtml.h 
 sed -i "" "s/unsigned char index_html/unsigned char index_html PROGMEM/" include/IndexHtml.h
 echo "#include <Arduino.h>\n\r$(cat include/IndexHtml.h)" > include/IndexHtml.h 
 
-xxd -i -n node_html ../html/node.html.gz > include/NodeHtml.h 
+xxd -i -n node_html ../html/node.min.html.gz > include/NodeHtml.h 
 sed -i "" "s/unsigned char index_html/unsigned char index_html PROGMEM/" include/NodeHtml.h
 echo "#include <Arduino.h>\n\r$(cat include/NodeHtml.h)" > include/NodeHtml.h 
 
@@ -24,8 +28,12 @@ xxd -i -n styles_min_css ../html/css/styles.min.css.gz > include/StylesMinCss.h
 sed -i "" "s/unsigned char styles_min_css/unsigned char styles_min_css PROGMEM/" include/StylesMinCss.h
 echo "#include <Arduino.h>\n\r$(cat include/StylesMinCss.h)" > include/StylesMinCss.h 
 
-rm -f  ../html/devices.html.gz
-rm -f  ../html/index.html.gz   
-rm -f  ../html/node.html.gz 
+rm -f  ../html/devices.min.html
+rm -f  ../html/index.min.html   
+rm -f  ../html/node.min.html 
+rm -f  ../html/css/styles.min.css
+rm -f  ../html/devices.min.html.gz
+rm -f  ../html/index.min.html.gz   
+rm -f  ../html/node.min.html.gz 
 rm -f  ../html/js/index.js.gz 
 rm -f  ../html/css/styles.min.css.gz 
