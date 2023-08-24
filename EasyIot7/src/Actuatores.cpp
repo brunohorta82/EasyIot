4d84922bdcacfeaea86d1f2a00a4cb76a3c61c91
Bruno Horta <brunohorta82@gmail.com>
2023-08-25 00:22:26 +0100

    control lights via web, gpio and mqtt, change debouce lib to button2

--- EasyIot7/src/Actuatores.cpp
+++ EasyIot7/src/Actuatores.cpp
@@ -13,15 +13,6 @@ String ConfigOnofre::controlSwitch(const char *id, SwitchStateOrigin origin, Str
 {
   return "DONE";
 }
-const String ActuatorT::getCurrentState()
-{
-  if (isCover())
-  {
-    return String(lastPercentage);
-  }
-  // TODO
-  return "TODO";
-}
 
 void shuttersWriteStateHandler(Shutters *shutters, const char *state, byte length)
 {
@@ -147,20 +138,49 @@ void switchesCallback(message_t const &msg, void *arg)
   case KNX_CT_WRITE:
   {
     uint16_t stateIdx = msg.data[1] | (msg.data[0] << 8);
-    s->changeState(SwitchStateOrigin::KNX, String(stateIdx).toInt()); // TODO
+    s->changeState(SwitchStateOrigin::KNX, String(stateIdx).toInt());
     break;
   }
   };
 }
-void mqttSwitchControl(SwitchStateOrigin origin, const char *topic, const char *payload)
+void pressed(Button2 &btn)
 {
-  for (auto &sw : config.actuatores)
-  {
-    if (strcmp(sw.writeTopic, topic) == 0)
-    {
-      sw.changeState(origin, String(payload).toInt()); // TODO
-    }
-  }
+  Serial.println("pressed");
+  config.controlFeature(SwitchStateOrigin::GPIO_INPUT, btn.getID(), SwitchState::TOGGLE);
+}
+void released(Button2 &btn)
+{
+  Serial.print("released: ");
+  Serial.println(btn.wasPressedFor());
+}
+void changed(Button2 &btn)
+{
+  Serial.println("changed");
+}
+void click(Button2 &btn)
+{
+  Serial.println("click\n");
+}
+void longClickDetected(Button2 &btn)
+{
+  Serial.println("long click detected");
+}
+void longClick(Button2 &btn)
+{
+  Serial.println("long click\n");
+}
+void doubleClick(Button2 &btn)
+{
+  Serial.println("double click\n");
+}
+void tripleClick(Button2 &btn)
+{
+  Serial.println("triple click\n");
+  Serial.println(btn.getNumberOfClicks());
+}
+void tap(Button2 &btn)
+{
+  Serial.println("tap");
 }
 void ActuatorT::setup()
 {
@@ -182,10 +202,13 @@ void ActuatorT::setup()
   }
   for (auto input : inputs)
   {
-    Bounce bounce = Bounce();
-    bounce.attach(input, INPUT_PULLUP);
-    bounce.interval(5);
-    inputsBounced.push_back(bounce);
+    Button2 button;
+    button.begin(input);
+    button.setID(id);
+    button.setPressedHandler(pressed);
+    button.setClickHandler(click);
+    button.setDoubleClickHandler(doubleClick);
+    buttons.push_back(button);
   }
   if (isKnxSupport())
   {
@@ -198,43 +221,35 @@ void ActuatorT::setup()
   }
 }
 
-const void ActuatorT::notifyState(bool dirty, const char *origin)
+void ActuatorT::notifyState(SwitchStateOrigin origin)
 {
-  if (strcmp("INTERNAL", origin) == 0)
-  {
-    return;
-  }
-  const String currentStateToSend = getCurrentState();
-#ifdef DEBUG_ONOFRE
-  Log.notice("%s %s current state: %s" CR, tags::actuatores, name, currentStateToSend.c_str());
-#endif
+  String stateStr = String(state);
+  // Notify by MQTT/Homeassistant
   if (mqttConnected())
   {
-    publishOnMqtt(readTopic, currentStateToSend.c_str(), true);
+    publishOnMqtt(readTopic, stateStr, false);
   }
+
+  // Notify by MQTT OnofreCloud
   if (cloudIOSupport)
   {
-    notifyStateToCloudIO(readTopic, currentStateToSend.c_str(), currentStateToSend.length());
+    notifyStateToCloudIO(readTopic, stateStr.c_str());
   }
-  sendToServerEvents(String(id), currentStateToSend.c_str());
-  if (strcmp("KNX", origin) != 0 && isKnxSupport())
-  {
-    // TODO knx.write_1byte_int(knx.GA_to_address(knxAddress[0], knxAddress[1], knxAddress[1]), statePoolIdx);
-  }
-  if (isKnxGroup())
-  {
-    for (auto &sw : config.actuatores)
-    {
 
-      if (sw.id == id)
-      {
-        sw.changeState(SwitchStateOrigin::INTERNAL, currentStateToSend.toInt());
-      }
-    }
+  // Notify by SSW Webpanel
+  sendToServerEvents(String(id), stateStr.c_str());
+
+  // Notify by KNX
+  if (SwitchStateOrigin::INTERNAL != origin && isKnxSupport())
+  {
+    knx.write_1byte_int(knx.GA_to_address(knxAddress[0], knxAddress[1], knxAddress[2]), state);
   }
 }
+
 ActuatorT *ActuatorT::changeState(SwitchStateOrigin origin, int state)
 {
+  if (this->state == state)
+    return this;
 #ifdef DEBUG_ONOFRE
   Log.notice("%s Name:      %s" CR, tags::actuatores, name);
   Log.notice("%s State:     %d" CR, tags::actuatores, state);
@@ -248,7 +263,7 @@ ActuatorT *ActuatorT::changeState(SwitchStateOrigin origin, int state)
   if (isCover())
   {
     int level = state;
-    if (level > 100)
+    if (level == SwitchState::STOP)
       shutter->stop();
     else
       shutter->setLevel(max(0, min(100, level)));
@@ -259,11 +274,26 @@ ActuatorT *ActuatorT::changeState(SwitchStateOrigin origin, int state)
     delay(1000);
     writeToPIN(outputs[0], LOW);
   }
-  else if (isLight() || isSwitch())
+  else if ((isLight() || isSwitch()))
   {
-    writeToPIN(outputs[0], state ? HIGH : LOW);
+    if (state == SwitchState::ON || state == SwitchState::OFF)
+      writeToPIN(outputs[0], state ? HIGH : LOW);
+    else
+      return this;
   }
   this->state = state;
+  this->notifyState(origin);
+  if (isKnxGroup())
+  {
+    for (auto &sw : config.actuatores)
+    {
+      if (sw.id == id)
+      {
+        sw.state = state;
+        sw.notifyState(SwitchStateOrigin::INTERNAL);
+      }
+    }
+  }
   return this;
 }
 
@@ -275,117 +305,9 @@ void ConfigOnofre::loopSwitches()
     {
       sw.shutter->loop();
     }
-
-    for (auto bounce : sw.inputsBounced)
+    for (auto &button : sw.buttons)
     {
-      bounce.read();
+      button.loop();
     }
-
-    /*  switch (sw.mode)
-      {
-      case SWITCH:
-        if (sw.primaryGpio != constantsConfig::noGPIO && sw.lastPrimaryGpioState != primaryGpioEvent)
-        {
-  #ifdef DEBUG_ONOFRE
-          Log.notice("%s 1 lastPrimaryGpioState %d primaryGpioEvent %d" CR, tags::switches, sw.lastPrimaryGpioState, primaryGpioEvent);
-  #endif
-          sw.rotateState();
-  #ifdef DEBUG_ONOFRE
-          Log.notice("%s 2 lastPrimaryGpioState %d primaryGpioEvent %d" CR, tags::switches, sw.lastPrimaryGpioState, primaryGpioEvent);
-  #endif
-        }
-        break;
-      case PUSH:
-        if (sw.primaryGpio != constantsConfig::noGPIO && !primaryGpioEvent && sw.lastPrimaryGpioState != primaryGpioEvent)
-        {
-          sw.rotateState();
-        }
-
-        break;
-      case GATE_SWITCH:
-      {
-        bool primaryStateGpioEvent = true;
-        bool secondaryStateGpioEvent = true;
-        if (sw.primaryStateGpio != constantsConfig::noGPIO)
-        {
-          primaryStateGpioEvent = readPIN(sw.primaryStateGpio);
-          if (sw.lastPrimaryStateGpioState != primaryStateGpioEvent)
-          {
-            sw.lastPrimaryStateGpioState = primaryStateGpioEvent;
-            if (primaryStateGpioEvent)
-            {
-              sw.statePoolIdx = constanstsSwitch::closeIdx;
-            }
-            else
-            {
-              sw.statePoolIdx = constanstsSwitch::openIdx;
-            }
-            sw.notifyState(true, "LOOP");
-          }
-        }
-        if (sw.secondaryStateGpio != constantsConfig::noGPIO)
-        {
-          secondaryStateGpioEvent = readPIN(sw.secondaryStateGpio);
-          if (sw.lastSecondaryStateGpioState != secondaryStateGpioEvent)
-          {
-            sw.lastSecondaryStateGpioState = secondaryStateGpioEvent;
-            if (secondaryStateGpioEvent)
-            {
-              sw.statePoolIdx = constanstsSwitch::openIdx;
-            }
-            else
-            {
-              sw.statePoolIdx = constanstsSwitch::closeIdx;
-            }
-            sw.notifyState(true, "LOOP");
-          }
-        }
-      }
-
-      break;
-      case DUAL_SWITCH:
-        if (strcmp(sw.family, constanstsSwitch::familyCover) == 0 && sw.primaryGpio != constantsConfig::noGPIO && sw.secondaryGpio != constantsConfig::noGPIO)
-        {
-          if (primaryGpioEvent && secondaryGpioEvent && (sw.lastPrimaryGpioState != primaryGpioEvent || sw.lastSecondaryGpioState != secondaryGpioEvent))
-          {
-            sw.changeState(STATES_POLL[sw.statePoolIdx == constanstsSwitch::openIdx ? constanstsSwitch::secondStopIdx : constanstsSwitch::firtStopIdx].c_str(), "DUAL_SWITCH_STOP");
-          }
-          else if (!primaryGpioEvent && sw.lastPrimaryGpioState != primaryGpioEvent)
-          {
-            sw.changeState(STATES_POLL[constanstsSwitch::openIdx].c_str(), "DUAL_SWITCH_OPEN");
-          }
-          else if (!secondaryGpioEvent && sw.lastSecondaryGpioState != secondaryGpioEvent)
-          {
-            sw.changeState(STATES_POLL[constanstsSwitch::closeIdx].c_str(), "DUAL_SWITCH_CLOSE");
-          }
-        }
-        break;
-      case DUAL_PUSH:
-        if (strcmp(sw.family, constanstsSwitch::familyCover) == 0 && sw.primaryGpio != constantsConfig::noGPIO && sw.secondaryGpio != constantsConfig::noGPIO)
-        {
-          if (!primaryGpioEvent && sw.lastPrimaryGpioState != primaryGpioEvent)
-          { // PUSHED
-            sw.changeState(STATES_POLL[sw.statePoolIdx == constanstsSwitch::openIdx ? constanstsSwitch::firtStopIdx : constanstsSwitch::openIdx].c_str(), "DUAL_PUSH");
-          }
-          if (!secondaryGpioEvent && sw.lastSecondaryGpioState != secondaryGpioEvent)
-          { // PUSHED
-            sw.changeState(STATES_POLL[sw.statePoolIdx == constanstsSwitch::closeIdx ? constanstsSwitch::secondStopIdx : constanstsSwitch::closeIdx].c_str(), "DUAL_PUSH");
-          }
-        }
-        break;
-      default:
-        break;
-      }
-      sw.lastPrimaryGpioState = primaryGpioEvent;
-      sw.lastSecondaryGpioState = secondaryGpioEvent;
-      if (stateTimeout(sw))
-      {
-
-  #ifdef DEBUG_ONOFRE
-        Log.notice("%s State Timeout set change switch to %s " CR, tags::switches, STATES_POLL[sw.statePoolIdx].c_str());
-  #endif
-        sw.changeState(STATES_POLL[findPoolIdx(sw.autoStateValue, sw.statePoolIdx, sw.family)].c_str(), "AUTO_TIMEOUT");
-      }
-      */
   }
 }
