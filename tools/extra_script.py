30964f85beef5874ac441c70ceb2c92b58e3953b
Hauzman <56076331+Hauzman@users.noreply.github.com>
2026-02-11 20:03:15 +0200

    build: apply local wifi overrides + dev version string
    (cherry picked from commit 91b7544e66bcd488ff1ac84c31bab5ee2d1965e4)

--- tools/extra_script.py
+++ tools/extra_script.py
@@ -1,4 +1,28 @@
-Import("env")
-my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+import datetime
+from SCons.Script import Import, DefaultEnvironment # type: ignore
 
-env.Replace(PROGNAME="ONOFRE_%s_%s" % (env["PIOENV"] , my_flags.get("CPPDEFINES")[0][1]))
+# Import the environment
+env = DefaultEnvironment()
+
+try:
+    my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+except Exception:
+    my_flags = {}
+
+# Extract version
+version = None
+for define in my_flags.get("CPPDEFINES", []):
+    if isinstance(define, (list, tuple)) and define[0] == "VERSION":
+        version = define[1]
+        if isinstance(version, str):
+            version = version.strip('"')
+
+if not version:
+    version = "unknown"
+
+# Generate firmware name with date in DD.MM.YYYY format
+timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
+firmware_name = f"Firmware_{env['PIOENV']}_{version} - {timestamp}"
+
+# Replace the program name
+env.Replace(PROGNAME=firmware_name)
