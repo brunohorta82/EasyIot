7d1b431b85dea62f9b4d6e5068212da9990019e5
Bruno <bruno.p.amado@gmail.com>
2026-09-04 21:57:17 +0100

    feat(aquadance): add web matrix sequencer, 2D fountain simulation, and HA integration

--- src/ConfigOnofre.cpp
+++ src/ConfigOnofre.cpp
@@ -12,6 +12,7 @@
 #include "CloudIO.h"
 #include "DeviceClock.h"
 #include "Irrigation.h"
+#include "AquaDance.h"
 #include "Persistence.h"
 #include <algorithm>
 
@@ -554,6 +555,7 @@ ConfigOnofre &ConfigOnofre::load()
   // After the features, never before: a program naming a valve this device does
   // not have is dropped, which can only be decided once the valves are known.
   irrigation.load();
+  aquadance.load();
 #ifdef DEBUG_ONOFRE
   Log.notice("%s Stored config loaded." CR, tags::config);
 
@@ -736,6 +738,10 @@ void ConfigOnofre::backup(JsonVariant &root)
   JsonVariant irrigationRoot = root["irrigation"].to<JsonObject>();
   irrigation.jsonBody(irrigationRoot);
   irrigationRoot.remove("running");
+
+  JsonVariant aquadanceRoot = root["aquadance"].to<JsonObject>();
+  aquadance.jsonBody(aquadanceRoot);
+  aquadanceRoot.remove("running");
 }
 
 ConfigOnofre &ConfigOnofre::save()
@@ -2161,6 +2167,7 @@ void ConfigOnofre::json(JsonVariant &root, bool allFields)
       if (a.isGardenValve())
       {
         irrigation.json(root);
+        aquadance.json(root);
         break;
       }
     }
@@ -2353,6 +2360,7 @@ void ConfigOnofre::loopActuators()
     }
   }
   irrigation.loop();
+  aquadance.loop();
   endFeatureAccess();
 }
 void ConfigOnofre::requestRestart()
